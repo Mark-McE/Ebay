3f1d0d8fe0bec303d688cb0645e8ed103506d6d3
Mark McElroy <mark.mcelroy@outlook.com>
2018-11-08 23:35:21 +0000

    added basic user input to Seller

--- src/AuctionClient/Seller.java
+++ src/AuctionClient/Seller.java
@@ -1,4 +1,37 @@
 package AuctionClient;
 
-public class Seller {
+import java.util.Scanner;
+
+public class Seller extends Client implements Runnable {
+
+  public Seller() {
+    super();
+  }
+
+  @Override
+  public void run() {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("usage: TODO");
+
+    while (true) {
+      String input = sc.next().toLowerCase().trim();
+      switch (input) {
+        case "create auction":
+          createAuction();
+          break;
+        default:
+          System.out.println("unknown command: " + input);
+          break;
+      }
+    }
+  }
+
+  private void createAuction() {
+    System.out.println("created new auction with id: 0");
+  }
+
+  public static void main(String[] args) {
+    new Seller().run();
+  }
 }
